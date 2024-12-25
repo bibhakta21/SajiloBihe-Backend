829147f7e0900a007419da67a4a98c5b3797118f
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-12-25 15:43:15 +0545

    Added Booking routes with endpoints for fetching and saving bookings

---
+++ routes/BookingRoute.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const router=express.Router();
+const {findAll, save} = require("../controller/BookingController");
+
+
+router.get("/",  findAll);
+router.post("/", save);
+
+
+module.exports=router;
