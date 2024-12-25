76c0eba3282e5863af62ed98c33c4bceeed4e7f4
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-12-25 15:44:09 +0545

    Integrated Booking routes into the main application and update API structure

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDb = require("./config/db");
 const CustomerRouter = require("./routes/CustomerRoutes");
 const VenueRouter = require("./routes/VenueRoute");
+const BookingRouter = require("./routes/BookingRoute");
 const AuthRouter = require("./routes/AuthRoutes");
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/customer", CustomerRouter);
 app.use("/api/venue", VenueRouter);
+app.use("/api/booking", BookingRouter);
 app.use("/api/auth", AuthRouter);
 
 const port = 3000;
