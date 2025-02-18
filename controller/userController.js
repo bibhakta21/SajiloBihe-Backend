468c268abca06f1f15ca014792c9601043a65fda
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-18 20:57:56 +0545

    added user retrieval endpoints in User controller

--- controller/userController.js
+++ controller/userController.js
@@ -68,3 +68,28 @@ exports.loginUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+
+// Get all users (Admin)
+exports.getAllUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
+
+// Get user by ID (Admin)
+exports.getUserById = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).json({ error: "User not found" });
+  res.json(user);
+};
+
+// Get current user details
+exports.getUserByMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password"); // Exclude the password field
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
