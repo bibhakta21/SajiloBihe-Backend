93e6491aa8e94de6052c57d9e1669bd288d47ba5
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-18 20:47:00 +0545

    updated User schema with new fields and improvements

--- model/User.js
+++ model/User.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true }, 
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" }, // Role-based access
+  avatar: { type: String, default: "" }, 
+  role: { type: String, enum: ["admin", "user"], default: "user" },
+  resetPasswordToken: { type: String },
+  resetPasswordExpire: { type: Date },
+  passwordChangedAt: { type: Date },
 });
 
 const User = mongoose.model("User", userSchema);
