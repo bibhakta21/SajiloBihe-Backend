e7a47363fdc5330b6ccdf4aecfe5daf76219b883
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 22:37:35 +0545

    Refactored Booking model (added status options)

--- model/Booking.js
+++ model/Booking.js
@@ -1,28 +1,10 @@
 const mongoose = require("mongoose");
 
-const bookingSchema = new mongoose.Schema({
-    customerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "customers",
-    },
-    venueId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "venues",
-    },
-    date: {
-        type: String,
-        required: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true,
-    },
+const BookingSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  venue: { type: mongoose.Schema.Types.ObjectId, ref: "Venue", required: true },
+  bookingDate: { type: Date, default: Date.now, required: true },
+  status: { type: String, enum: ["pending", "approved", "canceled"], default: "pending" },
 });
 
-const Booking = mongoose.model("Booking", bookingSchema);
-
-module.exports = Booking;
+module.exports = mongoose.model("Booking", BookingSchema);
